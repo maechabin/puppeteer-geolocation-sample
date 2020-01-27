b1543f211a6c7ae4573054a64b6793a932c1aa11
maeda <takanori@stroly.jp>
2020-01-27 15:54:42 +0900

    外部の設定ファイルを読み込んでコマンド実行できるように修正

--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,9 @@
 import * as puppeteer from 'puppeteer';
-import * as Constants from './constants';
+import * as commander from 'commander';
+import * as fs from 'fs';
 
 (async () => {
-  // 自動操作したいページのURL
-  const targetUrl = Constants.TARGET_URL;
-
-  //  puppeteerを起動
-  const browser = await puppeteer.launch({
-    headless: false, // Headlessモードで起動するかどうか
-    slowMo: 50, // 50ミリ秒スローモーションで実行する
-  });
-
-  // geolocationを有効化
-  const context = browser.defaultBrowserContext();
-  // await context.clearPermissionOverrides();
-  await context.overridePermissions(targetUrl, ['geolocation']);
-
-  // 新しい空のページを開く
-  const page = await browser.newPage();
-
-  // Google Analyticsをブロックする
-  // https://help.apify.com/en/articles/2423246-block-requests-in-puppeteer
-  await page.setRequestInterception(true);
-  page.on('request', request => {
-    const url = request.url();
-
-    const filters = ['google-analytics', 'hermes', 'mobileanalytics'];
-    const shouldAbort = filters.some(urlPart => url.includes(urlPart));
-    if (shouldAbort) {
-      request.abort();
-    } else {
-      request.continue();
-    }
-  });
-
-  // view portの設定
-  await page.setViewport({
-    width: 1200,
-    height: 600,
-  });
-
-  const latlngs = [
-    [21.28001716099308, 202.1704959869385],
-    [21.282165632083615, 202.16903887689116],
-    [21.28483422878972, 202.16735579073435],
-    [21.285697404178457, 202.16652799397707],
-    [21.284543066060998, 202.16328989714384],
-    [21.286815182224156, 202.16002464294436],
-    [21.28841467096632, 202.15933799743655],
-    [21.290254061510897, 202.15401649475098],
-    [21.291693568494672, 202.14955329895022],
-    [21.293133061383962, 202.1456050872803],
-    [21.29481245193988, 202.1420001983643],
-    [21.298091206774725, 202.13745117187503],
-    [21.299850508484006, 202.1368503570557],
-    [21.30216955583029, 202.1365070343018],
-  ];
-
-  // https://nitayneeman.com/posts/getting-to-know-puppeteer-using-practical-examples/#faking-geolocation
-  await page.setGeolocation({ latitude: latlngs[0][0], longitude: getLng(latlngs[0][1]) });
-
-  // Webページにアクセスする
-  await page.goto(targetUrl);
-  await page.waitForSelector('title');
-
-  for (const latlng of latlngs) {
-    await page.waitFor(500);
-    await page.setGeolocation({ latitude: latlng[0], longitude: getLng(latlng[1]) });
-  }
-
-  // ブラウザを終了する
-  await browser.close();
+  commander.parse(process.argv);
 
   function getLng(lng: number) {
     if (lng > 180) {
@@ -78,4 +11,73 @@ import * as Constants from './constants';
     }
     return lng;
   }
+
+  fs.readFile(commander.args[0], { encoding: 'utf8' }, async (err, file) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+
+    const data: {
+      readonly url: string;
+      readonly interval: number;
+      readonly latlngs: number[][];
+    } = JSON.parse(file);
+
+    // 自動操作したいページのURL
+    const targetUrl = data.url;
+
+    //  puppeteerを起動
+    const browser = await puppeteer.launch({
+      headless: false, // Headlessモードで起動するかどうか
+      slowMo: 50, // 50ミリ秒スローモーションで実行する
+    });
+
+    // geolocationを有効化
+    const context = browser.defaultBrowserContext();
+    // await context.clearPermissionOverrides();
+    await context.overridePermissions(targetUrl, ['geolocation']);
+
+    // 新しい空のページを開く
+    const page = await browser.newPage();
+
+    // Google Analyticsをブロックする
+    // https://help.apify.com/en/articles/2423246-block-requests-in-puppeteer
+    await page.setRequestInterception(true);
+    page.on('request', request => {
+      const url = request.url();
+
+      const filters = ['google-analytics', 'hermes', 'mobileanalytics'];
+      const shouldAbort = filters.some(urlPart => url.includes(urlPart));
+      if (shouldAbort) {
+        request.abort();
+      } else {
+        request.continue();
+      }
+    });
+
+    // view portの設定
+    await page.setViewport({
+      width: 1200,
+      height: 600,
+    });
+
+    const latlngs = data.latlngs;
+
+    // 現在地をセットする
+    // https://nitayneeman.com/posts/getting-to-know-puppeteer-using-practical-examples/#faking-geolocation
+    await page.setGeolocation({ latitude: latlngs[0][0], longitude: getLng(latlngs[0][1]) });
+
+    // Webページにアクセスする
+    await page.goto(targetUrl);
+    await page.waitForSelector('title');
+
+    for (const latlng of latlngs) {
+      await page.setGeolocation({ latitude: latlng[0], longitude: getLng(latlng[1]) });
+      await page.waitFor(data.interval);
+    }
+
+    // ブラウザを終了する
+    await browser.close();
+  });
 })();
